0d745cc7333e0150d332d60686a6859487c18178
O.Nixie <onixie@gmail.com>
2011-07-08 18:40:01 +0900

    add comments for misc.lisp and setup.lisp

--- quicklisp/misc.lisp
+++ quicklisp/misc.lisp
@@ -8,8 +8,6 @@
 
 (defun use-only-quicklisp-systems ()
   (asdf:initialize-source-registry
-   '(:source-registry :ignore-inherited-configuration))
-  (asdf:map-systems 'asdf:clear-system)
+   '(:source-registry :ignore-inherited-configuration)) ;对于sbcl来说，仅保留系统SBCL_HOME中的
+  (asdf:map-systems 'asdf:clear-system)	;对每个加载过的系统，调用clear-system从asdf系统加载列表中去掉
   t)
-
-
--- quicklisp/setup.lisp
+++ quicklisp/setup.lisp
@@ -5,45 +5,49 @@
        (*print-pretty* t)
        (*print-escape* nil)
        (prefix (make-string indent :initial-element #\Space)))
-   (pprint-logical-block (nil words :per-line-prefix prefix)
+   (pprint-logical-block (nil words :per-line-prefix prefix) ;用于格式化输出
      (pprint-fill *standard-output* (sort (copy-seq words) #'string<) nil))
    (fresh-line)
    (finish-output)))
 
 (defun recursively-install (name)
   (labels ((recurse (name)
-             (let ((system (find-system name)))
+             (let ((system (find-system name))) ;从所有enabled的dist中，寻找第一个名称为name的系统
                (unless system
                  (error "Unknown system ~S" name))
-               (ensure-installed system)
-               (mapcar #'recurse (required-systems system))
+               (ensure-installed system) ;首先查看系统的asdf文件是否存在，如果不存在就安装该系统，
+					;安装的时候，先查看release中的每个提供的系统是否都存在，
+					;如果有不存在的，就重新安装整个release。
+					;安装release包括下载release包,解压到software目录,记录每个系统到release的meta文件中
+					;以及记录每个系统到系统的meta文件中
+               (mapcar #'recurse (required-systems system)) ;对该系统依赖的其他系统进行安装
                name)))
-    (with-consistent-dists
+    (with-consistent-dists		;非常特别的设计! 固定住*dist-enumeration-functions*中包含的函数,使其返回的值一直保持该调用时的内容,以保证其body中的内容引用的dists不发生变化
       (recurse name))))
 
-(defclass load-strategy ()
+(defclass load-strategy ()		;相当于在quickload的时候的系统名称
   ((name
     :initarg :name
     :accessor name)
-   (asdf-systems
+   (asdf-systems			;需要加载的asdf系统,依赖
     :initarg :asdf-systems
     :accessor asdf-systems)
-   (quicklisp-systems
+   (quicklisp-systems			;需要通过quicklisp下载的 系统, 依赖
     :initarg :quicklisp-systems
     :accessor quicklisp-systems)))
 
-(defmethod print-object ((strategy load-strategy) stream)
+(defmethod print-object ((strategy load-strategy) stream) ;显示依赖的系统的个数,包括本地已经有的,和需要下载的
   (print-unreadable-object (strategy stream :type t)
     (format stream "~S (~D asdf, ~D quicklisp)"
             (name strategy)
             (length (asdf-systems strategy))
             (length (quicklisp-systems strategy)))))
 
-(defgeneric quicklisp-releases (strategy)
+(defgeneric quicklisp-releases (strategy) ;需要下载的那些系统的release
   (:method (strategy)
     (remove-duplicates (mapcar 'release (quicklisp-systems strategy)))))
 
-(defgeneric quicklisp-release-table (strategy)
+(defgeneric quicklisp-release-table (strategy) ;放在hashtable中
   (:method ((strategy load-strategy))
     (let ((table (make-hash-table)))
       (dolist (system (quicklisp-systems strategy))
@@ -63,25 +67,25 @@
   (let ((asdf-systems '())
         (quicklisp-systems '()))
     (labels ((recurse (name)
-               (let ((asdf-system (asdf:find-system name nil))
-                     (quicklisp-system (find-system name)))
-                 (cond (asdf-system
+               (let ((asdf-system (asdf:find-system name nil)) ;通过asdf找到的系统,表明在本地
+                     (quicklisp-system (find-system name)))    ;通过quicklisp找到的系统,表明是quicklisp支持的
+                 (cond (asdf-system			       ;如果asdf先找到,那么就表明本地存在,直接调用即可
                         (push asdf-system asdf-systems))
-                       (quicklisp-system
+                       (quicklisp-system ;否则表明需要下载
                         (push quicklisp-system quicklisp-systems)
-                        (dolist (subname (required-systems quicklisp-system))
+                        (dolist (subname (required-systems quicklisp-system)) ;循环寻找那些依赖的系统, 为什么asdf的情况下不需要呢? 
                           (recurse subname)))
                        (t
                         (error 'system-not-found
                                :name name))))))
       (with-consistent-dists
-        (recurse name)))
+        (recurse name)))		;通过此举,就能直到到底要加载那些系统,下载那些release了
     (make-instance 'load-strategy
                    :name name
                    :asdf-systems (remove-duplicates asdf-systems)
                    :quicklisp-systems (remove-duplicates quicklisp-systems))))
 
-(defun show-load-strategy (strategy)
+(defun show-load-strategy (strategy)	;显示要加载的本地系统,以及要下载的系统的release
   (format t "To load ~S:~%" (name strategy))
   (let ((asdf-systems (asdf-systems strategy))
         (releases (quicklisp-releases strategy)))
@@ -94,13 +98,13 @@
 
 (defvar *macroexpand-progress-in-progress* nil)
 
-(defun macroexpand-progress-fun (old-hook &key (char #\.)
-                                 (chars-per-line 50)
-                                 (forms-per-char 250))
-  (let ((output-so-far 0)
-        (seen-so-far 0))
-    (labels ((finish-line ()
-               (when (plusp output-so-far)
+(defun macroexpand-progress-fun (old-hook &key (char #\.) ;返回一个新的宏展开的hook, 在宏展开的同时,显示进度
+                                 (chars-per-line 50)	  ;每行显示50个字符最多
+                                 (forms-per-char 250))	  ;展开250个宏显示一个字符
+  (let ((output-so-far 0)				  ;当前一行已经显示的字符个数
+        (seen-so-far 0))				  ;已经展开的宏的个数
+    (labels ((finish-line ()				  ;用字符填充一行剩余的部分
+               (when (plusp output-so-far)		  ;如果没有输出过,这一行就不用填充了
                  (dotimes (i (- chars-per-line output-so-far))
                    (write-char char))
                  (terpri)
@@ -108,75 +112,75 @@
              (show-string (string)
                (let* ((length (length string))
                       (new-output (+ length output-so-far)))
-                 (cond ((< chars-per-line new-output)
+                 (cond ((< chars-per-line new-output) ;如果字符串显示的长度超过该行的结尾,那么重新还一行在显示
                         (finish-line)
                         (write-string string)
-                        (setf output-so-far length))
+                        (setf output-so-far length)) ;换行后剩余的字符
                        (t
-                        (write-string string)
+                        (write-string string) ;如果能显示的下,那么不还行就直接显示
                         (setf output-so-far new-output))))
                (finish-output))
              (show-package (name)
                ;; Only show package markers when compiling. Showing
                ;; them when loading shows a bunch of ASDF system
                ;; package noise.
-               (when *compile-file-pathname*
+               (when *compile-file-pathname* ;输出编译过程中的信息
                  (finish-line)
                  (show-string (format nil "[package ~(~A~)]" name)))))
-      (lambda (fun form env)
+      (lambda (fun form env)		;新的宏展开的hook
         (when (and (consp form)
-                   (eq (first form) 'cl:defpackage)
-                   (ignore-errors (string (second form))))
-	  (show-package (second form)))
-        (incf seen-so-far)
-        (when (<= forms-per-char seen-so-far)
+                   (eq (first form) 'cl:defpackage) ;显示包名! 如果是一个defpackage宏,并且第二个是包的名称,那么就显示这个信息. 
+		   (ignore-errors (string (second form))))
+	  (show-package (second form)));这招很灵,毕竟所有的系统都需要defpackage,所以通过它表明当前处理的是什么package
+        (incf seen-so-far)		;增加当前展开的宏的个数
+        (when (<= forms-per-char seen-so-far) ;如果展开的宏个数到达要求的时候,就显示一个字符,表明当前已经展开了这些宏
           (setf seen-so-far 0)
           (write-char char)
           (finish-output)
           (incf output-so-far)
-          (when (<= chars-per-line output-so-far)
+          (when (<= chars-per-line output-so-far) ;超过一行了,也是需要换行的
             (setf output-so-far 0)
             (terpri)
             (finish-output)))
-        (funcall old-hook fun form env)))))
+        (funcall old-hook fun form env))))) ;调用真正的hook函数
 
 (defun call-with-macroexpand-progress (fun)
-  (let ((*macroexpand-hook* (if *macroexpand-progress-in-progress*
+  (let ((*macroexpand-hook* (if *macroexpand-progress-in-progress* ;如果不在宏展开进度中, 那么就调用新的hook,显示展开的进度
                                 *macroexpand-hook*
                                 (macroexpand-progress-fun *macroexpand-hook*)))
-        (*macroexpand-progress-in-progress* t))
+        (*macroexpand-progress-in-progress* t)) ;表明已经在宏展开的进度显示中, 排他处理!
     (funcall fun)
     (terpri)))
 
 (defun apply-load-strategy (strategy)
-  (map nil 'ensure-installed (quicklisp-releases strategy))
-  (call-with-macroexpand-progress
+  (map nil 'ensure-installed (quicklisp-releases strategy)) ;保证需要的release都下载下来
+  (call-with-macroexpand-progress			    ;然后通过asdf加载strategy, 通过autoload-system-and-dependencies会自动加载他需要的那些系统
    (lambda ()
      (format t "~&; Loading ~S~%" (name strategy))
      (asdf:oos 'asdf:load-op (name strategy) :verbose nil))))
 
-(defun autoload-system-and-dependencies (name &key prompt)
-  (setf name (string-downcase name))
-  (with-simple-restart (abort "Give up on ~S" name)
-    (let ((strategy (compute-load-strategy name)))
-      (show-load-strategy strategy)
-      (when (or (not prompt)
+(defun autoload-system-and-dependencies (name &key prompt) ;犀利的递归, restart处理, 局部跳转, 一应俱全!
+  (setf name (string-downcase name))	;要加载的系统名称
+  (with-simple-restart (abort "Give up on ~S" name) ;如果出错了,可以放弃
+    (let ((strategy (compute-load-strategy name)))  ;计算所有需要加载的依赖系统,以及需要下载的那些
+      (show-load-strategy strategy)		    ;显示这些信息
+      (when (or (not prompt)			    
                 (press-enter-to-continue))
         (tagbody
          retry
          (handler-bind
-             ((asdf:missing-dependency
-               (lambda (c)
+             ((asdf:missing-dependency	;如果出现没有加载依赖包的错误(需求)
+               (lambda (c)		;那么处理该需求,加载那些需要的包.这个就是自动的,加载包及其依赖
                  (let ((parent (asdf::missing-required-by c))
                        (missing (asdf::missing-requires c)))
                    (when (typep parent 'asdf:system)
-                     (autoload-system-and-dependencies missing
+                     (autoload-system-and-dependencies missing ;对依赖包继续调用,以保证其上层的依赖也被这样自动处理
                                                        :prompt prompt)
-                     (go retry))))))
-           (apply-load-strategy strategy)))))
+                     (go retry))))))	;通过tagbody跳转,重新加载一次,在所有依赖都被加载后,重新加载将会成功
+           (apply-load-strategy strategy))))) ;自动加载这一切的开始
     name))
 
-(defvar *initial-dist-url*
+(defvar *initial-dist-url*		;发布的位置,我很想建镜像!
   "http://beta.quicklisp.org/dist/quicklisp.txt")
 
 (defun maybe-initial-setup ()
